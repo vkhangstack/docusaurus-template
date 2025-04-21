94a11d02e62e10e1eb466936ef318e6a8f173857
Pham Van Khang <phamvankhang.tvi@gmail.com>
2025-04-21 23:22:05 +0700

    chore: config gh-pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://vkhangstack.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docusaurus-template/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
